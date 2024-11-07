e50efb95a18b1023b0a0d9ba7db080a36423c3ce
Daniel Agustin Avila Cavieres <daniel.avila@ug.uchile.cl>
2024-11-06 22:18:23 -0300

    hito 2

---
+++ pudu-productions/scenes/checkpoint.gd
@@ -0,0 +1,13 @@
+class_name Checkpoint
+extends Area2D
+
+@export var respawn_point: Node2D
+
+func _ready() -> void:
+	body_entered.connect(_on_body_entered)
+
+
+func _on_body_entered(body: Node) -> void:
+	var player = body as Player
+	if player:
+		get_tree().change_scene_to_file("res://ui/win_menu.tscn")
