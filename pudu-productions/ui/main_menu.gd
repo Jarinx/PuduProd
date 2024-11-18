e7acc7e9179508a9357536ad45c6f3335363b5ca
Pamela Mendoza <pame.mendoza14.99@gmail.com>
2024-11-17 22:50:40 -0300

    credits screen
    credits screen

--- pudu-productions/ui/main_menu.gd
+++ pudu-productions/ui/main_menu.gd
@@ -15,7 +15,7 @@ func _on_start_pressed () -> void:
 	
 
 func _on_credits_pressed () -> void:
-	pass
+	get_tree().change_scene_to_file("res://ui/credits.tscn")
 
 func _on_quit_pressed () -> void:
 	get_tree().quit()
