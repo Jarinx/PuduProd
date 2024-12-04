5937890eaab3b52c140bbf49689477ece780e7aa
Daniel Agustin Avila Cavieres <daniel.avila@ug.uchile.cl>
2024-12-03 22:17:14 -0300

    win_condition

--- pudu-productions/ui/inventory.gd
+++ pudu-productions/ui/inventory.gd
@@ -7,16 +7,23 @@ func _ready() -> void:
 	slots = Global.get_inventory()
 	update_ui()
 func update_ui():
-	print(Global.get_inventory())
+
 	slots = Global.get_inventory()
-	print(slots)
-	print("Updating ui")
+
+
 	for i in range(len(slots)):
-		print("updating slots")
+
 		print(slots[i])
 		if slots[i] != null:
-			print("found slot")
+
 			print(slots[i].item_texture)
 			slot_images[i].texture = slots[i].item_texture
+func use_item():
+	for i in range(len(slots)):
+		if slots[i] != null:
+			slots[i]= null
+			slot_images[i].texture = null
+			update_ui()
+			break
 			
 	
