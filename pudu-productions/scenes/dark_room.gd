e62febf8d25d2bc2a1bf444976ea7ba8d1e9e8ae
Daniel Agustin Avila Cavieres <daniel.avila@ug.uchile.cl>
2024-12-06 04:17:27 -0300

    demo2-casi

--- pudu-productions/scenes/dark_room.gd
+++ pudu-productions/scenes/dark_room.gd
@@ -10,3 +10,7 @@ func _on_area_2d_area_entered(area: Area2D) -> void:
 	print(area.owner)
 	if area.owner.has_method("Player"):
 		area.owner.auto_iluminate(self)
+
+
+func _on_area_2d_2_area_entered(area: Area2D) -> void:
+	pass # Replace with function body.
--- pudu-productions/scenes/main.gd
+++ pudu-productions/scenes/main.gd
@@ -4,9 +4,11 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	Global.health= 3
 	hearts_container.setMaxHearts(Global.maxHealth)
 	hearts_container.updateHearts(player.currentHealth)
 	player.healthChanged.connect(hearts_container.updateHearts)
+	player.point_light_2d.enabled = true
 	$AudioStreamPlayer2D.play()
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
